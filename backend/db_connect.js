37f06af104e98c01b1a089057cd431350a1def8d
Clement31 <clement-lasnier@orange.fr>
2021-06-24 12:00:52 +0200

    Ajout fichier db_connect.js

---
+++ backend/db_connect.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
+
+});
+
+db.connect(function(error) {
+    if (error) {
+        return console.log({error: 'erreur ! '});
+    }
+    console.log("Connecté à la base de données MySQL!");
+
+});
+
+module.exports = db;
