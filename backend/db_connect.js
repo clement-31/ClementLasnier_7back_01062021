e7cac5a66846a3e303c70dc3daa40a59644df976
Clement31 <clement-lasnier@orange.fr>
2021-07-08 20:28:20 +0200

    Modifs contenu fichier db_connect

--- backend/db_connect.js
+++ backend/db_connect.js
@@ -2,7 +2,6 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE
@@ -13,7 +12,7 @@ console.log(process.env.DB_PASSWORD);
 
 db.connect(function(error) {
     if (error) {
-        return console.log({error: 'erreur ! '});
+        return console.log({error: error});
     }
     console.log("Connecté à la base de données MySQL!");
 
